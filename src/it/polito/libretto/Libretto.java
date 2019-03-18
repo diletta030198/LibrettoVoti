f6a7abc43688e0d9bf8a359c45cb73aecf63f873
dilet <dilet@LAPTOP-OPBM641F>
2019-03-18 18:01:38 +0100

    Aggiunto metodo add.

--- src/it/polito/libretto/Libretto.java
+++ src/it/polito/libretto/Libretto.java
@@ -1,5 +1,22 @@
 package it.polito.libretto;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Libretto {
 
+	private List <Voto> voti;
+
+	public Libretto() {
+		this.voti= new ArrayList<Voto>(); 
+	} 
+	
+	public void add (int voto, String corso,LocalDate data) {
+		
+	}
+public void add (Voto v) {
+		
+	}
+	
 }
